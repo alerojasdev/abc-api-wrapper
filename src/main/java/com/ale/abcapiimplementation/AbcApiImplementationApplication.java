fcc999dfca6bba3d8f83b7bd48a61671f2d213c9
alejandro rojas <alerojasdev@gmail.com>
2024-01-09 10:58:00 -0300

    Spring project initialized

---
+++ src/main/java/com/ale/abcapiimplementation/AbcApiImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.ale.abcapiimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcApiImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcApiImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ale/abcapiimplementation/AbcApiImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ale.abcapiimplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcApiImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
