d6afd12ae8871a5a10b73c97f7e1d36249f2381b
alejandro rojas <alerojasdev@gmail.com>
2024-01-11 16:36:24 -0300

    added interceptor to api path to check for api-key header

--- src/main/java/com/ale/abcapiimplementation/controller/AbcApiRestController.java
+++ src/main/java/com/ale/abcapiimplementation/controller/AbcApiRestController.java
@@ -1,14 +1,16 @@
 package com.ale.abcapiimplementation.controller;
 
+//import com.ale.abcapiimplementation.configuration.AuthenticationService;
 import com.ale.abcapiimplementation.entity.ErrorBodyMessage;
 import com.ale.abcapiimplementation.entity.NewsMetaData;
 import com.ale.abcapiimplementation.rest.AbcApiWrap;
 import com.ale.abcapiimplementation.rest.Base64Converter;
+//import io.swagger.annotations.ApiOperation;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-
 @RestController
 @RequestMapping("/abc-api-implementation")
 public class AbcApiRestController {
@@ -16,10 +18,28 @@ public class AbcApiRestController {
     AbcApiWrap abcApiWrap;
     @Autowired
     Base64Converter base64Converter;
-    @GetMapping(value = "/consulta", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_PLAIN_VALUE, MediaType.TEXT_HTML_VALUE})
-    private ResponseEntity getAbcInfo(@RequestParam(name = "requestInfo", required=false) String requestInfo, @RequestParam(name = "f", required = false) boolean f){
+
+//    @ApiOperation(value = "Retrieves a List of MetaData of the article requested", notes = "Provide a valid text")
+    @GetMapping(
+            value = "/consulta",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.TEXT_PLAIN_VALUE,
+                    MediaType.TEXT_HTML_VALUE
+            }
+    )
+    private ResponseEntity getAbcInfo(
+            @RequestParam(name = "requestInfo", required=false)
+            String requestInfo,
+            @RequestParam(name = "f", required = false)
+            Boolean base64Requested,
+            @RequestHeader("apiKey") String apiKey,
+            HttpServletRequest request
+    ){
 
         try {
+
             if (requestInfo == null || requestInfo.isEmpty() || requestInfo.isBlank()){
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                         .body(new ErrorBodyMessage("g268","Parámetros inválidos"));
@@ -27,7 +47,7 @@ public class AbcApiRestController {
 
             List<NewsMetaData> newsMetaData =  abcApiWrap.getAbcNewsData(requestInfo);
 
-            if (f){
+            if (Boolean.FALSE.equals(base64Requested)){
                 for (NewsMetaData dto : newsMetaData){
                     dto.setContenido_foto(base64Converter.imageUrlToBase64String(dto.getEnlace_foto()));
                 }
---
+++ src/main/java/com/ale/abcapiimplementation/security/ApiKeyInterceptor.java
@@ -0,0 +1,40 @@
+package com.ale.abcapiimplementation.security;
+
+import com.ale.abcapiimplementation.entity.ErrorBodyMessage;
+import com.ale.abcapiimplementation.repository.ApiKeyRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class ApiKeyInterceptor  implements HandlerInterceptor {
+
+    private static final String API_KEY_HEADER = "apiKey";
+
+    @Autowired
+    ApiKeyRepository apiKeyRepository;
+
+    private volatile String apiKey;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (apiKey == null) {
+            apiKey = apiKeyRepository.findAll().get(0).getCodeAccess();
+        }
+
+        String headerApiKey = request.getHeader(API_KEY_HEADER);
+        if (this.apiKey.equals(headerApiKey)) {
+            return true;
+        } else {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("application/json");
+            response.getWriter().write(new ObjectMapper().writeValueAsString(new ErrorBodyMessage("g103", "No Autorizado")));
+
+            return false;
+        }
+    }
+
+}
